9a68612a86e45a2112a7ef5a90893e96037ccccd
LasithEranga <lasitheranga1@gmail.com>
2022-09-23 13:19:38 +0530

    feat: pagination btn functionality for more advance cases

---
+++ NewFrontend/pagination.js
@@ -0,0 +1,30 @@
+const pagination = (noOfRows, rowsPerPage , offset = 0) => {
+  let contentHtml = "";
+  let btnArray = [];
+  let noOfPages = (noOfRows / rowsPerPage).toFixed(0);
+  if (noOfRows % rowsPerPage > 0) {
+    noOfPages++;
+  }
+  let start = 0;
+  let end = 0;
+
+  //create the array of elemets that contains the buttons
+  for (let i = 0; i < noOfPages; i++) {
+    btnArray.push(`<span  role="button" onclick="setTableRows(${i * rowsPerPage})" id="pg-${i}" class="mx-3 fw-bold notselected-page pg-btn text-primary  ">${i + 1}</span>`);
+  }
+
+  //  check no of pages
+  if (noOfPages > 3) {
+    if ((offset+1) === btnArray.length - 1) {
+      contentHtml += btnArray[offset] + btnArray[offset+1];
+    }else{
+        contentHtml += btnArray[offset] + btnArray[offset+1] + "..." + btnArray[btnArray.length - 1];
+    }
+  } else {
+    btnArray.map((btn) => {
+      contentHtml += btn;
+    });
+  }
+ 
+  return contentHtml;
+};
